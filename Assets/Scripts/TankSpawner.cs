188fa9c955787955a01341b06dadc82722c35270
Sajan Satheesh <64242770+Sajan-Satheesh@users.noreply.github.com>
2022-06-17 22:54:33 +0530

    Initial commit

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject TankSpawnerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(TankSpawnerPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
